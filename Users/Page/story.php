2f93191a65e69d33f8c26474546af4711bf92dbc
uyensiuxinhhh <uyen.nguyen23@student.passerellesnumeriques.org>
2022-03-25 21:43:31 +0700

    stories page

---
+++ Users/Page/story.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>museum</title>
+    <link rel="stylesheet" href="../Assets/css/reset.css">
+    <link rel="stylesheet" href="../Assets/css/museumStyle.css">
+</head>
+<body>
+<header class="u__background-color-greenblue">
+    <div class="u__header-top">
+        <div class="u__title u--font-weight-text u__font-family-text">Live for passion</div>
+        <div class="u__logo-text u__font-special-text">new stories</div>
+        <div></div>
+    </div>
+    <div class="u__header-bottom">
+        <ul class="u__menu-navigation">
+            <li class=" u__menu-navigation-text u__menu-text u__font-family-text"><a href="">the artist &dtrif;</a>
+                <ul>
+                    <li><a href="">the stories</a></li>
+                    <li><a href="">news</a></li>
+                    <li><a href="">Behind the scenes</a></li>
+                </ul>
+            </li>
+            <li class="u__menu-navigation-text u__menu-text u__font-family-text"><a href="">about us</a></li>
+            <li class="u__menu-navigation-text u__menu-text u__font-family-text"><a href="">wishlist</a></li>
+            <li class="u__menu-navigation-text u__menu-text u__font-family-text"><a href="">abc</a></li>
+            <li class="u__menu-navigation-text u__menu-text u__font-family-text"><a href="">special</a></li>
+        </ul>
+    </div>
+</header>
+<main>
+    <div class="container">
+        <div class="u__first-elementor-wrap u__font-family-text u__background-color-greenblue">
+            <div class="u__big-text">
+                <div class="u__big-text-1">Do </div>
+                <div>everything you want, rightnow!!</div>
+            </div>
+            <div></div>
+            <div class="u__first-quotes u__font-family-text1">
+                <div class="u__first-quotes-text">“Sing like no one’s listening, love like you’ve never been hurt, dance like nobody’s watching, and live like it’s heaven on earth.”</div>
+                <div class="u__first-quotes-author"></div
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="u__second-elementor-wrap">
+            <div class="u__single-image">
+                <img src="https://media.istockphoto.com/photos/fishing-boat-on-the-sea-picture-id1317183033?b=1&k=20&m=1317183033&s=170667a&w=0&h=yxqgt1iGZTifOWFeJCsubRx4epRXDlxK_3v1u0KDe5Y=" alt="">
+            </div>
+            <div></div>
+            <div class="u__index-image">
+                <img class="u__index-image-under" src="https://images.unsplash.com/photo-1529304344766-6b537de190f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHZpZXRuYW18ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="">
+                <img class="u__index-image-front" src="https://media.istockphoto.com/photos/green-tea-field-in-morning-picture-id510674530?k=20&m=510674530&s=612x612&w=0&h=ERRgthvUeMndL3Hd5CUR8rcAHPBqq6TQCsW7kTcnCk0=" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="u__third-elementor-wrap u__background-color-greenblue">
+        </div>
+    </div>
+    <div class="container">
+        <div class="u__text-slider u__background-color-greenblue">
+            <div class="u__text-slider-run">
+                Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. As with all matters of the heart, you’ll know when you find it
+            </div>
+        </div>
+    </div>
+</main>
+</body>
+<script src="../Assets/js/stories-js.js"></script>
+</html>
