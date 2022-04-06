75055159ada70e505cdf547a99e42a0d84eeb08c
A Công <acongkplpe@gmail.com>
2022-04-06 20:51:41 +0700

    uploat page footer

--- Users/Page/footer.php
+++ Users/Page/footer.php
@@ -6,41 +6,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Assets/css/footerStyle.css">
-    <link rel="stylesheet" href="../../Assets/css/reset.css">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
     <title>Document</title>
 </head>
 
 <body>
-    <div class="--c--fluit--footer">
-        <div class="--c--footer-">
-            <div class="--c--footer-one">
-                <h3>the artist</h3>
-                <p>Biography</p>
-                <p>Behind the scenes</p>
-                <p>News</p>
-            </div>
-            <div class="--c--footer-one">
-                <h3>project</h3>
-                <p>Portfolio</p>
-                <p>projects</p>
-                <p>Editorial</p>
-            </div>
-            <div class="--c--footer-one">
-                <h3>galleries</h3>
-                <p>Fine art Galleries in Hoi An</p>
-                <p>Fine art Galleries in Sai Gon</p>
+    <?php include_once "../Page/header.php"?>
 
+    <footer>
+        <div class="c__container">
+            <div class="left box">
+                <div class="upper">
+                    <div class="topic">About us</div>
+                    <p>CodingLab is a channel where you can learn HTML, CSS, and also JavaScript along with creative CSS
+                        Animations and Effects.</p>
+                </div>
+                <div class="lower">
+                    <div class="topic">Contact us</div>
+                    <div class="phone">
+                        <a href="#"><i class="fas fa-phone-volume"></i>+0079089 6767</a>
+                    </div>
+                    <div class="email">
+                        <a href="#"><i class="fas fa-envelope"></i>abc@gmail.com</a>
+                    </div>
+                </div>
             </div>
-            <div class="--c--footer-one">
-                <h3>other links</h3>
-                <p>Online store</p>
-                <p>Museum</p>
-                <p>Contact</p>
+            <div class="middle box">
+                <div class="topic">OTHER LINKS</div>
+                <div><a href="#">Online store</a></div>
+                <div><a href="#">Museum</a></div>
+                <div><a href="#">Contact</a></div>
+                <div><a href="#">Theme Development, Design</a></div>
+                <div><a href="#">Mobile Application Design</a></div>
+                <div><a href="#">Wire raming & Prototyping</a></div>
             </div>
-
+            <div class="right box">
+                <div class="topic">Subscribe us</div>
+                <form action="#">
+                    <input type="text" placeholder="Enter email address">
+                    <input type="submit" name="" value="Send">
+                    <div class="media-icons">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
+        <div class="c__bottom">
+            <p>Photo @<a href="#">Name author</a> All rights reserved
+            </p>
+        </div>
+    </footer>
 </body>
 
 </html>
